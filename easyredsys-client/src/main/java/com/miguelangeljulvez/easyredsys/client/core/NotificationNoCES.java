d91a1bc03f1f1f766f93fea83da086b5d9f0df6a
Miguel Ángel Júlvez <info@miguelangeljulvez.com>
2019-04-19 11:14:44 +0200

    Error en la firma devuelta por el servidor al enviar los datos de la tarjeta

--- easyredsys-client/src/main/java/com/miguelangeljulvez/easyredsys/client/core/NotificationNoCES.java
+++ easyredsys-client/src/main/java/com/miguelangeljulvez/easyredsys/client/core/NotificationNoCES.java
@@ -1,10 +1,12 @@
 package com.miguelangeljulvez.easyredsys.client.core;
 
+import org.json.JSONException;
 import sis.redsys.api.ApiWsMacSha256;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.xml.bind.annotation.XmlElement;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -36,8 +38,34 @@ public final class NotificationNoCES extends Notification {
         sb.append(getDs_MerchantCode());
         sb.append(getDs_Currency());
         sb.append(getDs_Response());
+        sb.append(getDs_CardNumber());
         sb.append(getDs_TransactionType());
         sb.append(getDs_SecurePayment());
+
+        return sb.toString();
+    }
+
+    @XmlElement(name = "Ds_CardNumber")
+    public String getDs_CardNumber() {
+        String ds_cardNumber = "";
+        try {
+            ds_cardNumber = apiMacSha256.getParameter("Ds_CardNumber");
+        } catch (JSONException e) {
+        }
+        return ds_cardNumber;
+    }
+
+    public void setDs_CardNumber(String ds_cardNumber) {
+        apiMacSha256.setParameter("Ds_CardNumber", ds_cardNumber);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(super.toString());
+        sb.append("Ds_CardNumber:");
+        sb.append(getDs_CardNumber());
+        sb.append(System.lineSeparator());
 
         return sb.toString();
     }
